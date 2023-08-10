76ce8a8f2b7c45aef737fbf1f24782ec4852c4e7
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2023-08-10 20:21:43 +0200

    Styles, fonts & images

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,12 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Logo from "./logo.png";
 
 const Navbar = () => {
   return (
     <nav>
+      <Image src={Logo} alt='Helpdesk Logo' width={70} quality={100} placeholder='blur' />
       <h1>Helpdesk</h1>
       <Link href='/'>Dashboard</Link>
       <Link href='/tickets'>Tickets</Link>
