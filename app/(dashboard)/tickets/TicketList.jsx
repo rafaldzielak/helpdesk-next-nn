cc6524f9c4acb33101b8e41dd148fdbf810a8309
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2023-08-17 07:59:21 +0200

    Fetch tickets from supabase DB

--- app/(dashboard)/tickets/TicketList.jsx
+++ app/(dashboard)/tickets/TicketList.jsx
@@ -1,13 +1,13 @@
 import Link from "next/link";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 
 async function getTickets() {
-  const res = await fetch("http://localhost:4000/tickets", {
-    next: {
-      revalidate: 0, // use 0 to opt out of using cache
-    },
-  });
+  const supabase = createServerComponentClient({ cookies });
 
-  return res.json();
+  const { data, error } = await supabase.from("Tickets").select();
+  if (error) console.log(error.message);
+  return data;
 }
 
 export default async function TicketList() {
