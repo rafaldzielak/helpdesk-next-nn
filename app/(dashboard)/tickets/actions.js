1e5c11cf8bcf14f0f8ca46fa981609a9d3103f88
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2023-08-24 18:10:49 +0200

    add ticket server action

--- app/(dashboard)/tickets/actions.js
+++ app/(dashboard)/tickets/actions.js
@@ -1,5 +1,21 @@
 "use server";
 
-export async function addTicket() {
-  console.log("addTicket");
+import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
+import { revalidatePath } from "next/cache";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export async function addTicket(formData) {
+  const ticket = Object.fromEntries(formData);
+
+  const supabase = createServerActionClient({ cookies });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  const { error } = await supabase.from("Tickets").insert({ ...ticket, user_email: session.user.email });
+  if (error) console.log({ error, data, status });
+
+  revalidatePath("/tickets");
+  redirect("/tickets");
 }
--- app/(dashboard)/tickets/create/CreateForm.jsx
+++ app/(dashboard)/tickets/create/CreateForm.jsx
@@ -1,48 +1,25 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import React from "react";
 import { useState } from "react";
 import { addTicket } from "../actions";
 
 const CreateForm = () => {
-  const router = useRouter();
-
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
-  const [priority, setPriority] = useState("low");
   const [isLoading, setIsLoading] = useState(false);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setIsLoading(true);
-    const ticket = { title, body, priority };
-    const res = await fetch("http://localhost:3001/api/tickets", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(ticket),
-    });
-    const json = await res.json();
-    if (json.error) console.log(json.error.message);
-    if (json.data) {
-      router.refresh();
-      router.push("/tickets");
-    }
-  };
 
   return (
     <form action={addTicket} className='w-1/2'>
       <label>
         <span>Title:</span>
-        <input required type='text' onChange={(e) => setTitle(e.target.value)} value={title} />
+        <input name='title' required type='text' />
       </label>
       <label>
         <span>Body:</span>
-        <textarea required onChange={(e) => setBody(e.target.value)} value={body} />
+        <textarea name='body' required />
       </label>
       <label>
         <span>Priority:</span>
-        <select onChange={(e) => setPriority(e.target.value)} value={priority}>
+        <select name='priority'>
           <option value='low'>Low Priority</option>
           <option value='medium'>Medium Priority</option>
           <option value='high'>High Priority</option>
